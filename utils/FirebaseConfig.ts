a1d78bb000cf961a804023804981d275c501c50e
Rita Trindade da Cruz <142616617+Ritatrcr@users.noreply.github.com>
2025-04-30 13:20:24 -0500

    firebase cnfiguration

--- utils/FirebaseConfig.ts
+++ utils/FirebaseConfig.ts
@@ -1,24 +1,20 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCuzTXMl1at07F8anls2058sqyOOn8uejc",
-  authDomain: "proyecto-final-dam-85abd.firebaseapp.com",
-  projectId: "proyecto-final-dam-85abd",
-  storageBucket: "proyecto-final-dam-85abd.firebasestorage.app",
-  messagingSenderId: "582895250432",
-  appId: "1:582895250432:web:fc0d51c529cf3010fa7ce5",
-  measurementId: "G-4KYR59Y1ZP"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app); 
-
 export const db = getFirestore(app);
 export const auth = getAuth(app);
